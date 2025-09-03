fd195fc84ce0fd33bcceb6d487609bcb8733a99e
oddpdev <ssk.acessorios310@gmail.com>
2025-09-03 20:35:17 -0300

    Add files via upload

---
+++ SOMA - AULA01.INT.JAVA-samuelhenrique_32525448.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Soma
+{
+	public static void main(String[] args) {
+	    Scanner scanner=new Scanner(System.in);
+		System.out.println("Informe um número: ");
+		int numeroUm=scanner.nextInt();
+		System.out.println("O número escolhido é: "+numeroUm);
+		
+		System.out.println("Informe um número: ");
+		int numeroDois=scanner.nextInt();
+		System.out.println("O número escolhido é: "+numeroDois);
+		
+		int resultado=(numeroUm+numeroDois);
+		System.out.println("O resultado é: "+resultado);
+		
+		scanner.close();
+	}
+}
